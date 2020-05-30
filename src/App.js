cc46d876bd4f6530db4c2c3bdd55a7b80b9fafde
Vadym Kaidalov <vadym.kaidalov@gmail.com>
2020-05-31 00:44:06 +0300

    Setup <BrowserRouter>, create mini-drawer <NavigationBar>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
+import { Switch, Route } from 'react-router-dom';
+import {makeStyles} from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+
+import NavigationBar from "./NavigationBar";
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+    toolbar: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+    },
+
+}));
 
 function App() {
-  return (
-    <div className="App">
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
-    </div>
-  );
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <NavigationBar />
+            <main className={classes.content}>
+                <div className={classes.toolbar} />
+                <Switch>
+                    <Route path="/me">
+                        <Typography paragraph>Profile page.</Typography>
+                    </Route>
+                    <Route path="/about">
+                        <Typography paragraph>About page.</Typography>
+                    </Route>
+                    <Route path="/">
+                        <Typography paragraph>Home page.</Typography>
+                    </Route>
+                </Switch>
+            </main>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/NavigationBar.js
@@ -0,0 +1,180 @@
+import React, { useState } from 'react';
+import { NavLink, withRouter } from 'react-router-dom';
+import clsx from "clsx";
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+    IconButton,
+    Drawer,
+    ListItemText,
+} from '@material-ui/core';
+import { createStyles, makeStyles, useTheme } from '@material-ui/core/styles';
+import Divider from "@material-ui/core/Divider";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+
+import './NavigationBar.css';
+
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) =>
+    createStyles({
+        appBar: {
+            zIndex: theme.zIndex.drawer + 1,
+            transition: theme.transitions.create(['width', 'margin'], {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.leavingScreen,
+            }),
+        },
+        appBarShift: {
+            marginLeft: drawerWidth,
+            width: `calc(100% - ${drawerWidth}px)`,
+            transition: theme.transitions.create(['width', 'margin'], {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+        },
+        menuButton: {
+            marginRight: 36,
+        },
+        hide: {
+            display: 'none',
+        },
+        drawer: {
+            width: drawerWidth,
+            flexShrink: 0,
+            whiteSpace: 'nowrap',
+        },
+        drawerOpen: {
+            width: drawerWidth,
+            transition: theme.transitions.create('width', {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+        },
+        drawerClose: {
+            transition: theme.transitions.create('width', {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.leavingScreen,
+            }),
+            overflowX: 'hidden',
+            width: theme.spacing(7) + 1,
+            [theme.breakpoints.up('sm')]: {
+                width: theme.spacing(9) + 1,
+            },
+        },
+        toolbar: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'flex-end',
+            padding: theme.spacing(0, 1),
+            // necessary for content to be below app bar
+            ...theme.mixins.toolbar,
+        },
+        navLink: {
+            textDecoration: 'none'
+        }
+    }),
+);
+
+const NavigationBar = (props) => {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleDrawerOpen = () => {
+        setIsOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setIsOpen(false);
+    };
+
+    const activeRoute = (routeName) => {
+        const pathname = props.location.pathname;
+        if (routeName === "/") {
+            return routeName === pathname;
+        }
+        return pathname.startsWith(routeName);
+    }
+
+    return (
+        <React.Fragment>
+            <AppBar
+                position="fixed"
+                className={clsx(classes.appBar, {
+                    [classes.appBarShift]: isOpen,
+                })}
+            >
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        className={clsx(classes.menuButton, {
+                            [classes.hide]: isOpen,
+                        })}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap>
+                        Optimus Type
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                variant="permanent"
+                className={clsx(classes.drawer, {
+                    [classes.drawerOpen]: isOpen,
+                    [classes.drawerClose]: !isOpen,
+                })}
+                classes={{
+                    paper: clsx({
+                        [classes.drawerOpen]: isOpen,
+                        [classes.drawerClose]: !isOpen,
+                    }),
+                }}
+            >
+                <div className={classes.toolbar}>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+                    </IconButton>
+                </div>
+                <Divider />
+                <List>
+                    <NavLink to="/" className={classes.navLink}>
+                        <ListItem button selected={activeRoute("/")}>
+                            <ListItemIcon><InboxIcon /></ListItemIcon>
+                            <ListItemText primary="Home" />
+                        </ListItem>
+                    </NavLink>
+                    <NavLink to="/me" className={classes.navLink}>
+                        <ListItem button selected={activeRoute("/me")}>
+                            <ListItemIcon><InboxIcon /></ListItemIcon>
+                            <ListItemText primary="Profile" />
+                        </ListItem>
+                    </NavLink>
+                </List>
+                <Divider />
+                <List>
+                    <NavLink to="/about" className={classes.navLink}>
+                        <ListItem button selected={activeRoute("/about")}>
+                            <ListItemIcon><InboxIcon /></ListItemIcon>
+                            <ListItemText primary="About" />
+                        </ListItem>
+                    </NavLink>
+                </List>
+            </Drawer>
+        </React.Fragment>
+    );
+};
+
+export default withRouter(NavigationBar);
