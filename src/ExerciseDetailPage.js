b40130876979e171822e82029808ff80a47bc9c6
Vadym Kaidalov <vadym.kaidalov@gmail.com>
2020-06-19 21:22:24 +0300

    Show fastest + my latest attempts on the exercise page

--- src/ExerciseDetailPage.js
+++ src/ExerciseDetailPage.js
@@ -10,15 +10,31 @@ import {
     DialogContentText,
     DialogActions,
     Grid,
+    Paper,
+    Table,
+    TableContainer,
+    TableBody,
+    TableHead,
+    TableRow,
+    TableCell,
     Typography
 } from "@material-ui/core";
 import DeleteIcon from '@material-ui/icons/Delete';
 import KeyboardIcon from '@material-ui/icons/Keyboard';
+import {makeStyles} from "@material-ui/core/styles";
 
 import ExerciseItemPaper from "./ExerciseItemPaper";
 
 
+const useStyles = makeStyles({
+    table: {
+        minWidth: 400,
+    },
+});
+
 export default function ExerciseDetailPage(props) {
+    const classes = useStyles();
+
     const {exerciseId} = useParams();
 
     const authToken = props.authToken;
@@ -27,6 +43,9 @@ export default function ExerciseDetailPage(props) {
     const [exercise, setExercise] = useState(null);
     const [isDeleted, setIsDeleted] = useState(false);
     const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
+
+    const [fastestAttempts, setFastestAttempts] = useState(null);
+    const [myLatestAttempts, setMyLatestAttempts] = useState(null);
 
     useEffect(() => {
         if (exercise === null) {
@@ -40,6 +59,53 @@ export default function ExerciseDetailPage(props) {
                     alert("An error happened while loading the exercise.");
                     console.log(error);
                 })
+        }
+    });
+
+    useEffect(() => {
+        if (fastestAttempts === null) {
+            axios.get(
+                `${process.env.REACT_APP_BACKEND_BASE_URL}/v1.0/fastest-attempts/`,
+                {
+                    params: {
+                        exercise: exerciseId,
+                        ordering: "time_spent"
+                    }
+                }
+            )
+                .then(res => {
+                    setFastestAttempts(res.data['results']);
+                })
+                .catch(error => {
+                    alert("An error happened while loading the fastest attempts.");
+                    console.log(error);
+                });
+        }
+    });
+
+    useEffect(() => {
+        if (authToken === null) {
+            return;
+        }
+
+        if (myLatestAttempts === null) {
+            axios.get(
+                `${process.env.REACT_APP_BACKEND_BASE_URL}/v1.0/attempts/`,
+                {
+                    params: {
+                        exercise: exerciseId,
+                        creator: userId,
+                        ordering: "-created_at"
+                    }
+                }
+            )
+                .then(res => {
+                    setMyLatestAttempts(res.data['results']);
+                })
+                .catch(error => {
+                    alert("An error happened while loading yout latest attempts.");
+                    console.log(error);
+                });
         }
     });
 
@@ -70,7 +136,7 @@ export default function ExerciseDetailPage(props) {
 
     if (isDeleted) {
         return (
-            <Redirect to={`/exercises`} />
+            <Redirect to={`/exercises`}/>
         );
     }
 
@@ -126,6 +192,65 @@ export default function ExerciseDetailPage(props) {
                     >
                         Start typing
                     </Button>
+                </Grid>
+                <Grid item container spacing={2}>
+                    <Grid item xs={12} md={6}>
+                        <Typography variant="h5">Fastest Attempts</Typography>
+                        {
+                            (fastestAttempts === null) ? <Typography>
+                                Loading...
+                            </Typography> : <TableContainer component={Paper}>
+                                <Table className={classes.table} aria-label="simple table">
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell>User</TableCell>
+                                            <TableCell align="right">Score&nbsp;(ms)</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {fastestAttempts.map((row) => (
+                                            <TableRow key={row['creator']['username']}>
+                                                <TableCell component="th" scope="row">
+                                                    {row['creator']['username']}
+                                                </TableCell>
+                                                <TableCell align="right">{row['timeSpent']}</TableCell>
+                                            </TableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                        }
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <Typography variant="h5">My Latest Attempts</Typography>
+                        {
+                            (authToken === null) ? <Typography>
+                                    Sign In to save and see your latest attempts.
+                                </Typography> :
+                                (myLatestAttempts === null) ? <Typography>
+                                    Loading...
+                                </Typography> : <TableContainer component={Paper}>
+                                    <Table className={classes.table} aria-label="simple table">
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell>User</TableCell>
+                                                <TableCell align="right">Score&nbsp;(ms)</TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {myLatestAttempts.map((row) => (
+                                                <TableRow key={row['creator']['username']}>
+                                                    <TableCell component="th" scope="row">
+                                                        {row['creator']['username']}
+                                                    </TableCell>
+                                                    <TableCell align="right">{row['timeSpent']}</TableCell>
+                                                </TableRow>
+                                            ))}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                        }
+                    </Grid>
                 </Grid>
             </Grid>
         </Container>
